1cf2ed3cf22e5928140e7590c5ddb10c94b7aa6e
samad <yy5841453@gmail.com>
2022-02-07 13:32:56 +0600

    class 17 added

---
+++ Tabs.js
@@ -0,0 +1,30 @@
+
+// Tabs
+
+const samad_tab_menu = document.querySelectorAll('.samad-tab-menu ul li a');
+const samad_tab_pane = document.querySelectorAll('.samad-tab-pane');
+
+
+samad_tab_menu.forEach(atag =>{
+    atag.addEventListener('click', function(e){
+        e.preventDefault();
+
+        samad_tab_menu.forEach(items =>{
+            items.classList.remove('active')
+        });
+        atag.classList.add('active');
+
+
+        samad_tab_pane.forEach(data =>{
+            data.classList.remove('active');
+            console.log(data)
+        });
+
+
+
+
+        let tabpane = document.querySelector(this.getAttribute('href'));
+        tabpane.classList.add('active')
+        
+    })
+})
