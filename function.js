f3cffbbb6ead7d89912a0d4c86acf415cf916e27
samad <yy5841453@gmail.com>
2022-02-14 20:32:57 +0600

    result app view added

--- function.js
+++ function.js
@@ -8,3 +8,91 @@ function datasend(key,data){
    let json = JSON.stringify(data);
    return localStorage.setItem(key,json)
 }
+
+// 1 GPA,GREADE,CGPA.. WITH CONSTRUCTOR CUNCTION
+
+
+
+function Result(){
+
+    // GPA calculator
+    this.GpaGradeCal = function(marks){
+          let gpa;
+          let grade;
+          if( marks >= 0 && marks <33 ){
+              gpa = 0.0;
+              grade = 'F'
+          }else if( marks >= 33 && marks <40 ){
+              gpa = 1.0;
+              grade = 'D';
+          }else if( marks >= 40 && marks <50 ){
+            gpa = 2.0;
+            grade = 'C';
+        }else if( marks >= 50 && marks <60 ){
+            gpa = 3.0;
+            grade = 'B';
+        }else if( marks >= 60 && marks <70 ){
+            gpa = 3.5;
+            grade = 'A-';
+        }else if( marks >= 70 && marks <80 ){
+            gpa = 4.0;
+            grade = 'A';
+        }else if( marks >= 80 && marks <100 ){
+            gpa = 5.0;
+            grade = 'A+';
+        }
+
+
+        return {
+            gpa : gpa,
+            grade : grade,
+        }
+    }
+
+    // Fainal Result
+    this.fainalResult = function( bn, en, math, rel, ss,cs ){
+        let cgpa;
+        let fainalGrade;
+        let color;
+
+        let totalCgpa = this.GpaGradeCal(bn).gpa + this.GpaGradeCal(en).gpa + this.GpaGradeCal(math).gpa + this.GpaGradeCal(rel).gpa + this.GpaGradeCal(ss).gpa + this.GpaGradeCal(cs).gpa;
+          
+        cgpa = totalCgpa / 6;
+
+        if( bn <33 || en <33 || math <33 || rel <33 || ss <33 || cs <33){
+            fainalGrade = 'F';
+            color = 'danger'
+        }else if( cgpa >= 0 && cgpa < 1 ){
+            fainalGrade = 'F'
+            color = 'danger'
+        }else if( cgpa >= 1 && cgpa < 2 ){
+            fainalGrade = 'D'
+            color = 'success'
+        }else if( cgpa >= 2 && cgpa < 3 ){
+            fainalGrade = 'C'
+            color = 'success'
+        }else if( cgpa >= 3 && cgpa < 3.5 ){
+            fainalGrade = 'B'
+            color = 'success'
+        }else if( cgpa >= 3.5 && cgpa < 4 ){
+            fainalGrade = 'A-'
+            color = 'success'
+        }else if( cgpa >= 4 && cgpa < 5 ){
+            fainalGrade = 'A'
+            color = 'success'
+        }else if( cgpa == 5){
+            fainalGrade = 'A+'
+            color = 'success'
+        }
+
+        return {
+            fainalCgpa : fainalGrade == 'F' ? 'Faidel' : cgpa.toFixed(2),
+            fainalGrade : fainalGrade,
+            color : color,
+        }
+
+    }
+}
+
+
+let resultin = new Result();
--- student.js
+++ student.js
@@ -18,7 +18,7 @@ student_form.addEventListener('submit', function(e){
     let social_science = student_form.querySelector('input[placeholder="Social Science"');
     let religion = student_form.querySelector('input[placeholder="Religion"');
 
-    if(name.value == '' || roll.value == '' || Student_class.value == '' || Photo.value == '' || gender.value == '' ||bangla.value == '' || english.value == '' || math.value == '' || science.value == '' || social_science.value == '' || religion.value == ''){
+    if(name.value == '' || roll.value == '' || Student_class.value == '' || Photo.value == '' || bangla.value == '' || english.value == '' || math.value == '' || science.value == '' || social_science.value == '' || religion.value == ''){
         alert_h4.innerHTML = `<p class="bg-danger p-3">all field are required</p>`
     }else{
         alert_h4.innerHTML = `<p class="bg-success p-3">Data stable!</p>`
@@ -52,7 +52,6 @@ student_form.addEventListener('submit', function(e){
     student_form.querySelector('input[placeholder="Roll Numder"]').value = '';
     student_form.querySelector('input[placeholder="Class"]').value = '';
     student_form.querySelector('input[placeholder="Photo"]').value = '';
-    student_form.querySelector('input[name="gender"]:checked').value = '';
     student_form.querySelector('input[placeholder="Bangla"').value = '';
     student_form.querySelector('input[placeholder="English"').value = '';
     student_form.querySelector('input[placeholder="Math"').value = '';
@@ -78,16 +77,115 @@ function result_show(){
              <td>A</td>
              <td>4.6</td>
              <td><img style="width:50px;height:50px;object-fit:cover" src="${data.Photo}" alt=""></td>
-             <td><button id="view" class="btn btn-info btn-sm">View</button></td>
-             <td><button id="delete" class="btn btn-danger btn-sm">Delete</button></td>
+             <td><button id="student-moda" class="btn btn-info btn-sm" onclick="calculat_result(${index})">View</button></td>
+             <td><button id="delete" onclick="delet(${index})" class="btn btn-danger btn-sm">Delete</button></td>
       </tr>
         `;
     });
-    all_list.innerHTML = dataa
+    all_list.innerHTML = dataa;
+   
+    
 };
 
-const delete_data = document.getElementById('delete');
+//const delete_data = document.getElementById('delete');
 
-delete_data.addEventListener('click', function(){
-    
+function delet(index){
+    let conf = confirm('Are you sre ?');
+    if(conf){
+        let resive_data = data_get('result');
+        resive_data.splice(index,1)
+        datasend('result',resive_data)
+        result_show()
+    }else{
+        return false;
+    }
+
+}
+
+const student_moda = document.getElementById('student-moda');
+const samad_modal = document.querySelector('.samad-modal');
+const data_show = document.querySelector('.data_show');
+
+student_moda.addEventListener('click', function(e){
+
+    samad_modal.style.display = 'flex';
+    samad_modal.nextElementSibling.classList.add('active');
+
+});
+
+samad_modal.addEventListener('click', function(e){
+    if(e.target == this){
+        samad_modal.style.display = 'none';
+    }else{
+        samad_modal.style.display = 'flex';
+    }
 })
+
+function calculat_result(index){
+    let resive_data = data_get('result');
+    //let data = '';
+    resive_data.map(data =>{
+        data_show.innerHTML = ` 
+        <img style="width: 250px; height: auto; text-align: center; border-radius: 4px;" src="${resive_data[index].Photo}" alt="">
+                <h2>abdus samad</h2>
+                <hr>
+                      <table class="table table-striped">
+                      <thead>
+                          <tr>
+                              <th>Subject</th>
+                              <th>Marks</th>
+                              <th>CGPA</th>
+                              <th>Grade</th>
+                              <th>CGPA</th>
+                              <th>Result</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                      <tr>
+                            <td>Bangla</td>
+                            <td>${resive_data[index].bangla}</td>
+                            <td>${resultin.GpaGradeCal(resive_data[index].bangla).gpa}</td>
+                            <td>${resultin.GpaGradeCal(resive_data[index].bangla).grade}</td>
+                            <td class="bg-${resultin.fainalResult(resive_data[index].bangla,resive_data[index].english,resive_data[index].math,resive_data[index].science,resive_data[index].social_science,resive_data[index].religion).color}">${resultin.fainalResult(resive_data[index].bangla,resive_data[index].english,resive_data[index].math,resive_data[index].science,resive_data[index].social_science,resive_data[index].religion).fainalCgpa}</td>
+                            <td class="bg-${resultin.fainalResult(resive_data[index].bangla,resive_data[index].english,resive_data[index].math,resive_data[index].science,resive_data[index].social_science,resive_data[index].religion).color}">${resultin.fainalResult(resive_data[index].bangla,resive_data[index].english,resive_data[index].math,resive_data[index].science,resive_data[index].social_science,resive_data[index].religion).fainalGrade}</td>
+                     </tr>
+                        <tr>
+                            <td>English</td>
+                            <td>${resive_data[index].english}</td>
+                            <td>${resultin.GpaGradeCal(resive_data[index].english).gpa}</td>
+                            <td>${resultin.GpaGradeCal(resive_data[index].english).grade}</td>
+                     </tr>
+                        <tr>
+                            <td>Math</td>
+                            <td>${resive_data[index].math}</td>
+                            <td>${resultin.GpaGradeCal(resive_data[index].math).gpa}</td>
+                            <td>${resultin.GpaGradeCal(resive_data[index].math).grade}</td>
+                     </tr>
+                        <tr>
+                            <td>science</td>
+                            <td>${resive_data[index].science}</td>
+                            <td>${resultin.GpaGradeCal(resive_data[index].science).gpa}</td>
+                            <td>${resultin.GpaGradeCal(resive_data[index].science).grade}</td>
+                     </tr>
+                        <tr>
+                            <td>social science</td>
+                            <td>${resive_data[index].social_science}</td>
+                            <td>${resultin.GpaGradeCal(resive_data[index].social_science).gpa}</td>
+                            <td>${resultin.GpaGradeCal(resive_data[index].social_science).grade}</td>
+                     </tr>
+                        <tr>
+                            <td>religion</td>
+                            <td>${resive_data[index].religion}</td>
+                            <td>${resultin.GpaGradeCal(resive_data[index].religion).gpa}</td>
+                            <td>${resultin.GpaGradeCal(resive_data[index].religion).grade}</td>
+                     </tr>
+                     </tbody>
+                  </table>   
+                    
+        `;
+    })
+
+    console.log(resive_data[index])
+
+
+}
